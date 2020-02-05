987f272b39e50869ae366507cddd6d29f4cd37f8
allikelly2511 <allikelly2511@icloud.com>
2020-02-05 10:33:41 -0500

    sup dawg

--- testShareTwo/ViewController.swift
+++ testShareTwo/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var sharingImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sharedPressed(_ sender: UIButton) {
+        if let storiesUrl = URL(string: "instagram-stories://share"){
+            if UIApplication.shared.canOpenURL(storiesUrl) {
+                guard let image = sharingImage.image else { return }
+                guard let imageData = image.pngData() else { return }
+                let pasteboardItems: [String: Any] = [
+                    "com.instagram.sharedSticker.stickerImage": imageData,
+                    "com.instagram.sharedSticker.backgroundTopColor": "#1dd1a1",
+                    "com.instagram.sharedSticker.backgroundBottomColor": "#1dd1a1"
+                ]
+                let pasteboardOptions = [
+                    UIPasteboard.OptionsKey.expirationDate: Date().addingTimeInterval(300)
+                ]
+                UIPasteboard.general.setItems([pasteboardItems], options: pasteboardOptions)
+                UIApplication.shared.open(storiesUrl, options: [:], completionHandler: nil)
+            } else {
+                print("User doesn't have Instagram on their device :( #loser")
+            }
+        }
+    }
+    
 }
 
